14b8a70cba006d77c4364ca853637c2c6d423e98
Jason Dumont <jason.dumont@UKL029.local>
2017-05-16 13:57:56 +0100

    Base pages and fonts added

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 'use strict';
 
 //required libraries
-const ejs = require('ejs'),
+const nunjucks = require('nunjucks'),
       bodyParser = require('body-parser'),
       path = require('path'),
       http = require('http'),
@@ -25,23 +25,34 @@ const twitter = new Twitter(twitterAuth);
 app.set('port', process.env.PORT || port); //set port
 app.set('view options', { layout: true });
 app.set('views', './views'); //set where to look for file render
-app.set('view engine', 'ejs'); //set the view engine
+app.set('view engine', 'nunjucks'); //set the view engine
+
+//nunjucks configuration
+nunjucks.configure('views', {
+   autoescape: true,
+   express: app   
+});
 
 //middleware setup
 app.use(bodyParser()); //parse the data comign in
 
-
 //Route handling
 //live streaming route
 app.get('/', function(req,res){
-      res.render('live');
+      res.render('live',{title:'Live Version', sec:'hello'});
 });
 
 //timelapse route
 app.get('/timelapse', function(req,res){
-      res.render('timelapse');
+      res.render('timelapse',{title:'Timelapse'});
 });
 
+//error handling 
+app.use(function(req,res,next){
+      res.status(404).render('error',{
+            message:'Unfortunately we cant find what your looking for!'//message to send through
+      });
+})
 
 //Prepare the server for listening
 server.listen(app.get('port'), function(){
